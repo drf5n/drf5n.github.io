03f59b369ea713cbf615b25daf468ed0874b8f51
David Forrest <drf5na@gmail.com>
2022-01-10 13:44:37 -0500

    2022-01-10

--- update.bash
+++ update.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp -a ~/2020/Home/School/YCSD_covid_metrics/va_counties_map.html ./
-cp -a ~/2020/Home/School/YCSD_covid_metrics/us_covid_states_map.html ./
-cp -a ~/2020/Home/School/YCSD_covid_metrics/YorkCountyCovidMetric_plot.html ./
+cp -a ~/2020/Home/School/YCSD_covid_metrics/docs/va_counties_map.html ./
+cp -a ~/2020/Home/School/YCSD_covid_metrics/docs/us_covid_states_map.html ./
+cp -a ~/2020/Home/School/YCSD_covid_metrics/docs/YorkCountyCovidMetric_plot.html ./
